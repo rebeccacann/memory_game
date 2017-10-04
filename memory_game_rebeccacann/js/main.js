21444742b28651005b6fc7f454127a88968d9e01
rebulba <30329874+rebulba@users.noreply.github.com>
2017-10-03 22:07:30 -0400

    created game logic

---
+++ memory_game_rebeccacann/js/main.js
@@ -0,0 +1,8 @@
+// Desktop version
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardFour);
